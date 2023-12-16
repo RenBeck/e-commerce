078026ee3622e0277a49972c46fa0ce4da4292a5
RenBeck <renorb03@gmail.com>
2023-12-16 17:28:25 -0300

    models Sacola

--- sacola/admin.py
+++ sacola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ItemSacolaInline(admin.TabularInline):
+    model = models.ItemSacola
+    extra = 1
+
+
+class SacolaAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemSacolaInline
+    ]
+
+
+admin.site.register(models.Sacola, SacolaAdmin)
+admin.site.register(models.ItemSacola)
